492966ffd09ea8a898472bffb67fb0ecc80975af
thetalemon <sasami0314@gmail.com>
2024-10-27 17:33:34 +0900

    head, footerなどの調整

---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+export const myDomain = 'photo.manasas.dev'
+export const myUrl = `https://${myDomain}`
+export const myMainUrl = `https://manasas.dev`
+export const myName = 'manasas'
+export const mySiteName = 'とあるアルバム'
+export const mySiteDefaultDescription = '北極の倉庫に眠るアルバムたち'
+export const myCopyLight = `© 2024 ${myName}`
+export const myDefaultOgpImage = `${myUrl}/defaultOgp.png`
