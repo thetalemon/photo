c3d193e265558c0c7b10e9276e706541a86055a6
thetalemon <sasami0314@gmail.com>
2024-11-02 19:00:52 +0900

    script追加

---
+++ script/imports.py
@@ -0,0 +1,28 @@
+# importとPhotoの原形を書いてくれるスクリプト
+import os
+import glob
+import re
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+# ファイル一覧を取得
+files = glob.glob('../src/pages/photo/2024/10/22/*.jpg')
+sorted_files = sorted(files, key=natural_keys)
+
+# import用
+with open('imports.txt', 'w') as f: 
+  for i, file in enumerate(sorted_files):
+    name = os.path.basename(file)
+    photoNumber = name.split('.')[0].split('_')[2]
+    f.write('import Photo' + photoNumber + " from './" + name + "'\n")
+
+# html用 インデントはprettierに任せる
+with open('html.txt', 'w') as f: 
+  for i, file in enumerate(sorted_files):
+    name = os.path.basename(file)
+    photoNumber = name.split('.')[0].split('_')[2]
+    f.write('<Photo\n  img={Photo' + photoNumber + '}\n  alt=""\n  text=""\n  size={}\n/>\n')
--- script/rename.py
+++ script/rename.py
@@ -1,21 +1,9 @@
-
-
-# 特定のフォルダからファイル一覧を取得して命名変更する
+# 一括リネームするときのスクリプト
 import os
 import glob
 
-# ファイル一覧を取得
 files = glob.glob('../src/pages/photo/2024/10/22/*.jpg')
-
-# # ファイル一覧を表示
-# for file in files:
-#     # print(file)
     
-# ファイル名を変更して書き出す
 for file in files:
   new_name = '_' + os.path.basename(file)
-  print(new_name)
-  # 出力先は元のディレクトリ
   os.rename(file, os.path.dirname(file) + '/' + new_name)
-  # os.rename(file, new_name)
-  
